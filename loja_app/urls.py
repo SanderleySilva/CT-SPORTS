7ba04e3a9f118596a892ddd0772e61b70eb6c89b
Sanderley <sandernascimento98@gmail.com>
2025-04-06 23:14:32 -0300

    Adicionado categorias

--- loja_app/urls.py
+++ loja_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.lista_produtos, name = 'lista_produtos'),
-    path('info_produtos/<int:id>/',views.info_produtos, name = 'info_produtos'),
-
+    path('', views.lista_produtos, name='lista_produtos'),
+    path('info_produtos/<int:id>/', views.info_produtos, name='info_produtos'),
+    path('categorias/', views.produtos_por_categoria, name='produtos_por_categoria'),
 ]
--- loja_app/views.py
+++ loja_app/views.py
@@ -11,3 +11,16 @@ def lista_produtos(request):
 def info_produtos(request,id):
     produtos = get_object_or_404(Produto,id=id)
     return render(request, 'produtos/info_produtos.html', {'produto':produtos})
+
+def produtos_por_categoria(request):
+    categorias = Categoria.objects.all()
+    contexto = {}
+
+    for categoria in categorias:
+        produtos = Produto.objects.filter(categoria=categoria)
+        if produtos.exists():
+            contexto[categoria] = produtos
+
+    return render(request, 'produtos/produtos_por_categorias.html', {
+        'categorias_com_produtos': contexto
+    })
