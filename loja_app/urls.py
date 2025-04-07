c911b531d24b1833793fb7db8e22ea3931c145e1
Sanderley <sandernascimento98@gmail.com>
2025-04-07 17:18:49 -0300

    Adicionado área de pesquisa

--- loja_app/urls.py
+++ loja_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.lista_produtos, name='lista_produtos'),
     path('info_produtos/<int:id>/', views.info_produtos, name='info_produtos'),
     path('categorias/', views.produtos_por_categoria, name='produtos_por_categoria'),
+    path('busca/', views.buscaProduto, name='busca_produtos'),
 ]
+
--- loja_app/views.py
+++ loja_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Produto, Categoria
-
+from django.db.models import Q
 
 
 def lista_produtos(request):
@@ -24,3 +24,14 @@ def produtos_por_categoria(request):
     return render(request, 'produtos/produtos_por_categorias.html', {
         'categorias_com_produtos': contexto
     })
+
+
+
+
+def buscaProduto(request):
+    termo = request.GET.get('q')  # Obtém o termo da busca
+    produtos = Produto.objects.filter(
+        Q(nome__icontains=termo) | Q(categoria__nome__icontains=termo)
+    ) if termo else Produto.objects.none()  # Verifica se o termo foi fornecido
+
+    return render(request, 'produtos/busca_produtos.html', {'produtos': produtos, 'termo': termo})
